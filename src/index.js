4b649abc43013fbf81586a7554f00e47d16149a8
Raman Marсulevich <dwarf.md@gmail.com>
2023-08-04 22:12:04 +0200

    feaf: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let ret = n.toString();
+	let buf = '';
+	for(let i = ret.length-1; i >= 0; i--){
+		buf = buf + ret[i]
+	}
+	let num = parseInt(buf);
+	return num;
 }
